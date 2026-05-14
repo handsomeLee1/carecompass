e0236e2f7e6eed703bec4bc424da6dcdb126c9e5
handsomeLee1 <assetmaster1@gmail.com>
2026-05-14 01:56:47 +0000

    API Route 추가 + CORS 해결

---
+++ api/search.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { sido, sigungu, type } = req.query;
+  const API_KEY = '3097212a7319caf4dd8b9b52b3eb53ee88768616811b57cb7d870a418af57d99';
+  
+  const params = new URLSearchParams({
+    serviceKey: API_KEY,
+    pageNo: 1,
+    numOfRows: 50,
+    addrSido: sido || '',
+    addrSigungu: sigungu || '',
+    _type: 'json'
+  });
+  
+  if (type) params.append('longTermCareInstGbCd', type);
+  
+  try {
+    const response = await fetch(`https://apis.data.go.kr/B550928/searchLtcInsService/getLtcInsInfo?${params}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+}
