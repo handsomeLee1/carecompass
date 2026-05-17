875253fba34ec8723a190508e5d8f4d71e04f0b8
handsomeLee1 <assetmaster1@gmail.com>
2026-05-17 10:06:21 +0000

    공통 헤더 header.js로 통합

---
+++ header.js
@@ -0,0 +1,40 @@
+(function(){
+  var path=window.location.pathname;
+  var isCalc=path.includes('calculator');
+  var isFacility=path.includes('facility-cost');
+  var isHomecare=path.includes('homecare-cost');
+  var isCost=isFacility||isHomecare;
+
+  var subNav='';
+  if(isCalc){
+    subNav='<div class="tab-bar"><div class="tab-inner">'+
+      '<button class="tab-btn'+(path.includes('calculator')?' active':'')+'" onclick="switchCalc&&switchCalc(\'재가급여\')">재가급여 간편계산</button>'+
+      '<button class="tab-btn" onclick="switchCalc&&switchCalc(\'시설급여\')">시설급여 간편계산</button>'+
+      '<button class="tab-btn" onclick="switchCalc&&switchCalc(\'등급예상\')">등급예상 점수계산 <span class="calc-tab-badge">인기</span></button>'+
+      '</div></div>';
+  } else if(isCost){
+    subNav='<div class="tab-bar"><div class="tab-inner">'+
+      '<a href="/facility-cost.html" class="tab-btn'+(isFacility?' active':'')+'">요양원 입소 비용</a>'+
+      '<a href="/homecare-cost.html" class="tab-btn'+(isHomecare?' active':'')+'">방문요양 비용</a>'+
+      '<a href="#" class="tab-btn">주간보호 비용</a>'+
+      '<a href="#" class="tab-btn">복지용구 안내</a>'+
+      '</div></div>';
+  }
+
+  var html=
+    '<nav class="topnav">'+
+    '<div class="topnav-inner">'+
+    '<a href="/" class="logo">Care<span>Compass</span></a>'+
+    '<div class="nav-links">'+
+    '<a href="/"'+((!isCalc&&!isCost)?' class="active"':'')+'>기관찾기</a>'+
+    '<a href="/facility-cost.html"'+(isCost?' class="active"':'')+'>비용안내</a>'+
+    '<a href="/calculator.html"'+(isCalc?' class="active"':'')+'>계산기</a>'+
+    '<a href="#">장기요양 안내</a>'+
+    '</div>'+
+    '<div class="nav-right">국민건강보험공단 공공데이터</div>'+
+    '</div>'+
+    '</nav>'+
+    subNav;
+
+  document.write(html);
+})();
